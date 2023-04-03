0245dd023727546f45e68247f9216e4ae32024f7
mbattaloglu <mmbattaloglu@gmail.com>
2023-04-03 16:51:22 +0300

    Slicing

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -4,15 +4,18 @@ public class Blade : MonoBehaviour
 {
     private Camera mainCamera;
     private Collider bladeCollider;
+    private TrailRenderer bladeTrail;
     private bool slicing;
 
     public Vector3 direction { get; private set; }
+    public float sliceForce;
     public float minSliceVelocity = 0.01f;
 
     private void Awake()
     {
         mainCamera = Camera.main;
         bladeCollider = GetComponent<Collider>();
+        bladeTrail = GetComponentInChildren<TrailRenderer>();
     }
 
     private void Update()
@@ -50,12 +53,15 @@ public class Blade : MonoBehaviour
 
         slicing = true;
         bladeCollider.enabled = true;
+        bladeTrail.enabled = true;
+        bladeTrail.Clear();
     }
 
     private void StopSlicing()
     {
         slicing = false;
         bladeCollider.enabled = false;
+        bladeTrail.enabled = false;
     }
 
     private void ContinueSlicing()
